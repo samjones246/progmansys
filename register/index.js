d9ed0d87e201f5754e4fc1a359475e38e8e620b4
samjones246 <sam.jones240699@gmail.com>
2020-01-25 20:18:29 +0000

    initial commit

---
+++ register/index.js
@@ -0,0 +1,65 @@
+module.exports = async function (context, req) {
+    context.log('Register event fired');
+
+    if (req.query.firstName && 
+        req.query.lastName && 
+        req.query.email &&
+        req.query.password 
+        || ( req.body && 
+        req.body.firstName &&
+        req.body.lastName &&
+        req.body.email &&
+        req.body.password
+        )) 
+    {
+        var firstName = req.query.firstName || req.body.firstName
+        var lastName = req.query.lastName || req.body.lastName
+        var email = req.query.email || req.body.email
+        var password = req.query.password || req.body.password
+        var userExists = false;
+        var university = null;
+        var domain = email.split('@')[1];
+        for(i=0;i<users.length;i++){
+            if(users[i].email == email){
+                userExists = true;
+                break;
+            }
+        }
+        for(i=0;i<universities.length;i++){
+            if(universities[i].domain == domain){
+                university = i;
+                break;
+            }
+        }
+        if(userExists){
+            context.res = {
+                status: 409,
+                body: "A user with this email already exists"
+            }
+        }else if(university==null){
+            context.res = {
+                status: 400,
+                body: "Email domain does not match a registered university"
+            }
+        }else{
+            outputDocument = {
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password,
+                university: university,
+                about: "Nothing here yet"
+            }
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: "User account " + email + " created successfully"
+            };
+        }
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+}
