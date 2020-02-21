01805d751423b8b4772e2d66876db63d6806303d
samjones246 <sam.jones240699@gmail.com>
2020-02-21 21:47:36 +0000

    did things

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,47 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./progmansys-creds.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://progmansys-6f164.firebaseio.com"
+});
+const firestore = admin.firestore();
+exports.createProgramme = functions.https.onRequest(async (req, res) => {
+    const name = req.body.name;
+    const duration = req.body.duration;
+    const idToken = req.body.idToken;
+    /*
+    admin.auth().verifyIdToken(idToken).then(function(decodedToken) {
+        let uid = decodedToken.uid;
+        const programme = {
+            name: name,
+            leader: firestore.doc("users/"+uid),
+            duration: duration,
+            modules: [],
+            outcomes: [],
+            core: []
+        }
+    }).catch(function(error) {
+        // Handle error
+    });
+    */
+    firestore.collection('programmes').add({
+        name: name,
+        leader: firestore.doc("users/"+idToken),
+        duration: duration,
+        modules: [],
+        outcomes: [],
+        core: [],
+        administrators: [
+            firestore.doc("users/"+idToken)
+        ],
+        mapping: []
+    }).then(doc => {
+        res.send(doc);
+    });
+});
