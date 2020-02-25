3199877dd8a92f87884629810202c17eae5071ce
samjones246 <sam.jones240699@gmail.com>
2020-02-25 13:37:19 +0000

    added createModule (untested)

--- functions/index.js
+++ functions/index.js
@@ -50,3 +50,43 @@ exports.createProgramme = functions.https.onRequest(async (req, res) => {
         return;
     });
 });
+
+exports.createModule = functions.https.onRequest(async (req,res) => {
+    const name = req.body.name;
+    const semester = req.body.semester;
+    const year = req.body.year;
+    const idToken = req.body.idToken;
+    if(semester.isNaN || semester < 1 || semester > 2){
+        res.send("Semester must be a number between 1 and 2");
+        return;
+    }
+    if(year.isNaN || year < 1 || year > 4){
+        res.send("Year must be a number between 1 and 4");
+        return;
+    }
+    admin.auth().verifyIdToken(idToken).then(function(decodedToken) {
+        let uid = decodedToken.uid;
+        firestore.collection('modules').where('name', '==', name).get().then(snapshot => {
+            if(snapshot.empty){
+                firestore.collection('modules').add({
+                    name: name,
+                    leader: uid,
+                    semester: semester,
+                    year: year,
+                    outcomes: [],
+                }).then(documentRef => {
+                    documentRef.get().then(doc => {
+                        res.send(doc.data());
+                        return;
+                    })
+                });
+            }else{
+                res.send("A module with this name already exists");
+                return;
+            }
+        })
+    }).catch(function(error) {
+        res.status(403).send("Authentication Failed");
+        return;
+    });
+});
