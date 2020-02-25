7b843779705d5edb66b3f9c2b2cb519537bed3b9
samjones246 <sam.jones240699@gmail.com>
2020-02-25 13:14:36 +0000

    createProgramme fully implemented

--- functions/index.js
+++ functions/index.js
@@ -15,25 +15,38 @@ exports.createProgramme = functions.https.onRequest(async (req, res) => {
     const name = req.body.name;
     const duration = req.body.duration;
     const idToken = req.body.idToken;
+    if(duration.isNaN() || duration < 1 || duration > 4){
+        res.send("Duration must be a number between 1 and 4");
+        return;
+    }
     admin.auth().verifyIdToken(idToken).then(function(decodedToken) {
         let uid = decodedToken.uid;
-        firestore.collection('programmes').add({
-            name: name,
-            leader: uid,
-            duration: duration,
-            modules: [],
-            outcomes: [],
-            core: [],
-            administrators: [
-                uid
-            ],
-            mapping: []
-        }).then(documentRef => {
-            documentRef.get().then(doc => {
-                res.send(doc.data());
-            })
-        });
+        firestore.collection('programmes').where('name', '==', name).get().then(snapshot => {
+            if(snapshot.empty){
+                firestore.collection('programmes').add({
+                    name: name,
+                    leader: uid,
+                    duration: duration,
+                    modules: [],
+                    outcomes: [],
+                    core: [],
+                    administrators: [
+                        uid
+                    ],
+                    mapping: []
+                }).then(documentRef => {
+                    documentRef.get().then(doc => {
+                        res.send(doc.data());
+                        return;
+                    })
+                });
+            }else{
+                res.send("A programme with this name already exists");
+                return;
+            }
+        })
     }).catch(function(error) {
         res.status(403).send("Authentication Failed");
+        return;
     });
 });
