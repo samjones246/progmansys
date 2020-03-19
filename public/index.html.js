1bed6f874041c9b36022393d14f70198b5a12012
Sam Jones <sam.jones240699@gmail.com>
2020-03-19 16:45:48 +0000

    lots of frontend

--- public/index.html.js
+++ public/index.html.js
@@ -1,6 +1,7 @@
 // FirebaseUI config.
 var uiConfig = {
-    signInSuccessUrl: 'index.html',
+    signInFlow: 'popup',
+    signInSuccessUrl: '/',
     signInOptions: [
       firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
@@ -9,18 +10,162 @@ var uiConfig = {
 };
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 const apiRoot = "http://localhost:5001/progmansys-6f164/us-central1"
+const ProgrammeList = {
+    data: function(){
+        return {
+            user: null,
+            programmes: []
+        }
+    },
+    methods: {
+        getProgrammes: function () {
+            firebase.firestore().collection("programmes").where("administrators", "array-contains", this.user.uid).get()
+            .then(snapshot => {
+                snapshot.docs.forEach(doc => {
+                    var data = doc.data();
+                    data.id=doc.id;
+                    this.programmes.push(data);
+                })
+                return;
+            })
+        }
+    },
+    created: function(){
+        firebase.auth().onAuthStateChanged(user => {
+            this.user = user;
+            if(user){
+                this.getProgrammes();
+            }
+        })
+    },
+    template: 
+    `
+    <div>
+        <section class="hero is-info">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        Programmes
+                    </h1>
+                    <h2 class="subtitle">
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <section v-for="p in programmes" class="container is-primary is-light">
+            <br>
+            <p class="title">{{ p.name }}</p>
+            <p class="subtitle">
+                <router-link v-bind:to="'programmes/'+p.id">Edit</router-link> - <a>View</a>
+            </p>
+        </section>
+    </div>
+    `
+}
+const ModuleList = {
+    data: function(){
+        return {
+            user: null,
+            modules: []
+        }
+    },
+    methods: {
+        getModules: function () {
+            firebase.firestore().collection("modules").get()
+            .then(snapshot => {
+                snapshot.docs.forEach(doc => {
+                    this.modules.push(doc.data());
+                })
+                return;
+            })
+        }
+    },
+    created: function(){
+        firebase.auth().onAuthStateChanged(user => {
+            this.user = user;
+            if(user){
+                this.getModules();
+            }
+        })
+    },
+    template: 
+    `
+    <div>
+        <section class="hero is-info">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        Modules
+                    </h1>
+                    <h2 class="subtitle">
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <section v-for="m in modules" class="container is-primary is-light">
+            <br>
+            <p class="title">{{ m.name }}</p>
+            <p class="subtitle">
+                <a>Edit</a> - <a>View</a>
+            </p>
+        </section>
+    </div>
+    `
+}
+const Programme = {
+    data: function() {
+        return {
+            programme: null,
+            leader: null,
+            modules: null,
+            administrators: null,
+            ready: false
+        }
+    },
+    created: function() {
+        firebase.firestore().collection("programmes").doc(this.$route.params.id).get()
+        .then(snapshot => {
+            this.programme = snapshot.data();
+            this.ready = true;
+        })
+    },
+    template:
+    `
+    <div v-if="ready">
+        <section class="hero is-primary">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        {{ programme.name }}
+                    </h1>
+                    <h2 class="subtitle">
+                    </h2>
+                </div>
+            </div>
+        </section>
+    </div>
+    `
+}
+const Module = {
+
+}
+
+const routes = [
+    { path: '/programmes', component: ProgrammeList },
+    { path: '/modules', component: ModuleList },
+    { path: '/programmes/:id', component: Programme },
+]
+const router = new VueRouter({
+    routes
+})
 var app = new Vue({
     el: '#app',
+    router,
     data: {
         functions: functionsSpec,
-        selected: null,
         user: null,
-        response: ""
-    },
-    computed: {
-        loggedIn: function () {
-            return user ? true : false
-        }
+        showCreateProgramme: false,
+        showCreateModule: false
     },
     methods: {
         convertString: function (input) {
@@ -64,11 +209,21 @@ var app = new Vue({
                   this.response = error.message;
                 }
             });
-        }
+        },
+        logout: function () {
+            firebase.auth().signOut();
+            location.reload()
+        },
+        login: function () {
+            ui.start('#firebaseui-auth-container', uiConfig);
+        },
     },
     created: function(){
         firebase.auth().onAuthStateChanged(user => {
             this.user = user;
         })
+        if (ui.isPendingRedirect()) {
+            ui.start('#firebaseui-auth-container', uiConfig);
+        }
     }
 });
